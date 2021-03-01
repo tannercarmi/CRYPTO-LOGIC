5a4d4bf4f1c1afdbfb533529f5a6837e3d8dd23d
tannercarmi <78119747+tannercarmi@users.noreply.github.com>
2021-02-28 22:22:27 -0600

    Added .py main class and wordList.txt for program

---
+++ CRYPTO-LOGIC.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import random
+
+# CRYPTO-LOGIC
+
+# Open file to read
+inputFile = open("C:\\Users\\tanto\\Desktop\\wordList.txt","r")
+line = inputFile.readline()
+lst = []
+
+while line != "" :
+    line = inputFile.readline()
+    line = line.rstrip()
+    lst.append(line)
+
+inputFile.close()
+
+# Select random word from list    
+value = random.randint(0,len(lst))
+secretword = lst[value]
+
+# Split word into list of char
+shufflelist = list(secretword)
+
+# Shuffle char for secretword
+random.shuffle(shufflelist)
+
+# GAME START
+counter = 0     # counter implemented for incorrect guesses
+guesslist = []      # list created to compare to secretword
+i = 0
+guessdisplay=''         # string created to display current guessed letters
+
+# Menu for game with secretword as sentinel boolean
+while guessdisplay != secretword :
+    print("\nWelcome to CRYPTO-LOGIC!")
+    print("Try to guess the scrambled word, one letter at a time!")
+    print()
+    print("Scrambled word: %s" % shufflelist)
+    guess = input("Enter your guess ... ")      # asks for user input
+    if guess == secretword[i] :
+        guesslist.append(guess)
+        guessdisplay =''.join(guesslist)
+        i+=1
+        print()         # If guessed correctly
+        print("------------------------------------------")
+        print()
+        print("Incorrect guesses: %s" % counter)
+        print("Letters already guessed: %s" % guessdisplay)
+        print()
+        print("------------------------------------------")
+        print()
+    else :
+        counter+=1
+        print()         # If guessed incorrectly
+        print("------------------------------------------")
+        print()
+        print("Incorrect guesses: %s" % counter)
+        print("Letters already guessed: %s" % guessdisplay)
+        print()
+        print("-------------------------------------------")
+        print()
+
+print("\nCongratulations! You found the word after %s incorrect guess(es)!" % counter)
